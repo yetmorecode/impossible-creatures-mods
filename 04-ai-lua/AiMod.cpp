bafa4b789c13ed77da46089b7e1c4ab5d60684b2
yetmorecode <yetmorecode@posteo.net>
2022-04-16 01:47:04 +0200

    more ai

--- 04-ai-lua/AiMod.cpp
+++ 04-ai-lua/AiMod.cpp
@@ -1,9 +1,10 @@
 #include "../SDKHelper.h"
 
-static int lua_hello(lua_State* s) {
+static int __stdcall lua_hello(lua_State* s) {
+    dbTracefAux("%s", __FUNCTION__);
     //double d = lua_tonumber(s, 1);  /* get argument */
-    lua_pushstring(s, "Hello Lua");  /* push result */
-    return 1;  /* number of results */
+    //lua_pushnumber(s, 1337.0);  /* push result */
+    return 0;  /* number of results */
 }
 
 class AiCpu : public WrappedCpuInterface {
@@ -11,6 +12,7 @@ public:
     AiCpu(DLLCpuInterface* o) : WrappedCpuInterface(o) {}
     
     void InitLuaAI(LuaConfig* lc) {
+        dbTracefAux("%s", __FUNCTION__);
         original->InitLuaAI(lc);
         lc->RegisterCFunc("hello", &lua_hello);
         dbTracefAux("%s: Added %s::hello()", __FUNCTION__, lc->GetName());
@@ -22,6 +24,7 @@ class AiGame : public WrappedGameInterface {
 public:
     AiGame(DLLGameInterface* o) : WrappedGameInterface(o) {}
     DLLCpuInterface* GetCpuInterface() {
+        dbTracefAux("%s", __FUNCTION__);
         return new AiCpu(original->GetCpuInterface());
     }
 };
@@ -31,6 +34,7 @@ public:
 	AiMod(DLLInterface* o) : WrappedDllInterface(o) {}
 
     const wchar_t* GetName() {
+        dbTracefAux("%s", __FUNCTION__);
         static wchar_t name[64];
         wcscpy(name, original->GetName());
         wcscat(name, L" AiMod");
@@ -38,6 +42,7 @@ public:
     }
 
     DLLGameInterface* GameCreate(SimEngineInterface* sim) {
+        dbTracefAux("%s", __FUNCTION__);
         return new AiGame(original->GameCreate(sim));
     }
 };
